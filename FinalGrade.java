65f07a6b482734c9f6ae13f588ff226c1b7720d6
gnix22 <gjnix22@gmail.com>
2026-01-16 10:47:34 -0500

    set up framework for project

---
+++ FinalGrade.java
@@ -0,0 +1,6 @@
+public class FinalGrade { // class name if public* must match file name to properly compile.... 
+                          // (* I'm not sure just yet public is requirement for this)
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
